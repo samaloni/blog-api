8a4f10a1c1a2febf4e5d21edca23f74f9ebd3d5c
Amuthan Arulraj <arulraj.amuthan@gmail.com>
2017-01-24 08:07:22 -0500

    Adding base spring boot project

---
+++ src/main/java/org/sample/employee/EmployeeDirectoryApiApplication.java
@@ -0,0 +1,12 @@
+package org.sample.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeDirectoryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeDirectoryApiApplication.class, args);
+	}
+}
