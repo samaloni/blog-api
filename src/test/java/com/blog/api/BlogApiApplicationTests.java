1e080f9c8ae87a568d8d9558f4c0d3f795b2691c
Amuthan Arulraj <arulraj.amuthan@gmail.com>
2017-01-26 02:01:43 -0500

    Updating tests

--- src/test/java/com/blog/api/BlogApiApplicationTests.java
+++ src/test/java/com/blog/api/BlogApiApplicationTests.java
@@ -1,4 +1,4 @@
-package org.sample.employee;
+package com.blog.api;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class EmployeeDirectoryApiApplicationTests {
+public class BlogApiApplicationTests {
 
 	@Test
 	public void contextLoads() {
