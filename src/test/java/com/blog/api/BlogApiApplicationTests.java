49743c0ef9add90db1c208dc1ca45a1030c14d40
saloni <aloni.s@husky.neu.edu>
2017-01-31 19:56:52 -0500

    changed

--- src/test/java/com/blog/api/BlogApiApplicationTests.java
+++ src/test/java/com/blog/api/BlogApiApplicationTests.java
@@ -2,12 +2,40 @@ package com.blog.api;
 
 import static org.testng.Assert.*;
 
+import org.apache.http.entity.ContentType;
 import org.testng.annotations.Test;
+
+import com.blog.api.domain.User;
+
+
 
 public class BlogApiApplicationTests {
 
 	@Test
+	public void test_posts_users_api(){
+		HttpResponse<JsonNode> jsonResponse;
+		User user = new User();
+		user.setFirstName("Shrey");
+		user.setLastName("Panse");
+		user.setUsername("spanse");
+		
+		try{
+			jsonResponse = Unirest.post("http://localhost:8080/blog/api/users")
+					.header("accept", ContentType.APPLICATION_JSON.getMimeType())
+					.body(mapper.wrteValueAsString(user))
+					.asJson();
+			
+			assertNotNull(jsonResponse);
+		}catch(UnirestException e){
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+	@Test
 	public void contextLoads() {
+		
 	}
 
 }
